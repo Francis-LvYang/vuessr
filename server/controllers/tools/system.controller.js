9498fa3621b36018ae901c62ab34f7231f06459f
Francis Yang <1473074667@qq.com>
2019-05-29 19:31:01 +0800

    ss6

--- server/controllers/tools/system.controller.js
+++ server/controllers/tools/system.controller.js
@@ -1,2 +1,41 @@
 const os = require('os')
 const mongoose = require('mongoose')
+const pkg = require('../../../package.json')
+const globalConfig = require('../../config/global.config')
+
+exports.system = async (req, res, next) => {
+  const mongodbAdmin = new mongoose.mongo.Admin(mongoose.connection.db)
+  const db = await mongodbAdmin.buildInfo()
+  try {
+    res.json({
+      code: 200,
+      data: {
+        version: pkg.version,
+        osType: os.type(),
+        osRelease: os.release(),
+        nodeVersion: process.versions.node,
+        databaseVersion: db.version,
+      }
+    })
+  } catch (error) {
+    res.json({
+      code: 404,
+      data: '系统消息获取失败',
+      error
+    })
+  }
+}
+
+exports.globalConfig = async (req, res, next) => {
+  res.json({
+    code: 403,
+    data: {
+      siteName: globalConfig.app.siteName,
+      domain: globalConfig.app.domain,
+      seo: globalConfig.seo,
+      copyRight: '2014-2019 ©️ Essay',
+      isConfigEmail: !!(globalConfig.email.user && globalConfig.email.pass),
+      isConfigGithub: !!(globalConfig.github.id && globalConfig.github.secret),
+    }
+  })
+}
