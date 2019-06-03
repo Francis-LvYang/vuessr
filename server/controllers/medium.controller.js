69fabd58f9f809ccbc346593a49d8f8a53c33e45
Francis Yang <1473074667@qq.com>
2019-06-03 20:29:53 +0800

    ssr

--- server/controllers/medium.controller.js
+++ server/controllers/medium.controller.js
@@ -74,3 +74,107 @@ exports.deleteMedium = async (req, res, next) => {
     })
   }
 }
+
+exports.getMediumCategories = async (req, res, next) => {
+  try {
+    let data = await Category.find({ flag: 2 }).exec()
+    const arr = await Medium.aggregate([
+      { $group: { _id: '$category', total: { $sum: 1 } } }
+    ]).exec()
+    data = JSON.parse(JSON.stringify(data))
+    data = data.map(category => {
+      const t = arr.find(item => String(item._id) === String(category.id))
+      return {
+        ...category,
+        total: t ? t.total : 0
+      }
+    })
+    res.json({
+      code: 200,
+      data
+    })
+  } catch (error) {
+    res.json({
+      code: 404,
+      data: '媒体库获取失败',
+      error
+    })
+  }
+}
+
+exports.getMediumCategory = async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const data = await Category.findOne({
+      _id: id
+    }).exec()
+    res.json({
+      code: 200,
+      data
+    })
+  } catch (error) {
+    res.json({
+      code: 404,
+      data: '分类详情获取失败',
+      error
+    })
+  }
+}
+
+exports.postMediumCategory = async (req, res, next) => {
+  const { body } = req
+  body.flag = 2
+  try {
+    const data = await new Category(body).save()
+    res.json({
+      code: 200,
+      data
+    })
+  } catch (error) {
+    res.json({
+      code: 404,
+      data: '媒体库分类创建失败',
+      error
+    })
+  }
+}
+
+exports.patchMediumCategory = async (req, res, next) => {
+  const {
+    id,
+  } = req.params
+  let { body } = req
+  try {
+    body = await Category.findByIdAndUpdate(id, body).exec()
+    res.json({
+      success: true,
+      data: body,
+    })
+  } catch (error) {
+    res.json({
+      success: false,
+      message: '媒体库分类更新失败',
+      error,
+    })
+  }
+}
+
+exports.deleteMediumCategory = async (req, res, next) => {
+  const {
+    id,
+  } = req.params
+
+  try {
+    const body = await Category.findByIdAndRemove(id).exec()
+    res.json({
+      success: true,
+      data: body,
+    })
+  } catch (error) {
+    res.json({
+      success: false,
+      message: '媒体库删除失败',
+      error,
+    })
+  }
+}
